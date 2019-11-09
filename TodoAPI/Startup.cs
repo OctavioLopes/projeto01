4040751f9d148fd461c3efd2cc642f8e66d25195
OCTÁVIO HENRIQUE MARTINS LOPES <1911516518@acad.dvuadmin.net>
2019-11-09 15:55:33 -0300

    finalizaçao

--- TodoAPI/Startup.cs
+++ TodoAPI/Startup.cs
@@ -51,6 +51,11 @@ namespace TodoAPI
             }
 
             app.UseHttpsRedirection();
+
+            app.UseCors(c => c.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+                
             app.UseMvc();
         }
     }
